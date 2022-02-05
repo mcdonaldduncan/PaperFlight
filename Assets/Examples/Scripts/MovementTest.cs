7803e3fcda4096ad0e3c30b014442643b642f80b
mcdonaldduncan <380939@colum.edu>
2022-02-04 23:05:02 -0600

    Update MovementTest.cs

--- Assets/Examples/Scripts/MovementTest.cs
+++ Assets/Examples/Scripts/MovementTest.cs
@@ -8,17 +8,21 @@ using Liminal.SDK.VR.Avatars;
 using Liminal.SDK.VR.Input;
 using UnityEngine;
 
+/* Testing movement and rotation options
+ * Only axis inputs allowed are joysticks
+ * Using non emulated environment tomorrow so I can map input to rotation and directional changes 
+ */
+
 public class MovementTest : MonoBehaviour
 {
     [SerializeField] Transform paperPlane;
-    [SerializeField] Transform Cube;
-
     [SerializeField] float speed = 1.0f;
     [SerializeField] float moveValue = 1.0f;
     [SerializeField] float rotationSpeed = 1.0f;
 
     bool moveRight = false;
     bool moveLeft = false;
+    bool shouldRotate = true;
 
     float minRotation = -45;
     float maxRotation = 45;
@@ -28,7 +32,6 @@ public class MovementTest : MonoBehaviour
     private void Start()
     {
         neutralRotation = paperPlane.localRotation.eulerAngles;
-
     }
 
     private void Update()
@@ -38,11 +41,7 @@ public class MovementTest : MonoBehaviour
             return;
 
         var rightInput = GetInput(VRInputDeviceHand.Right);
-        var leftInput = GetInput(VRInputDeviceHand.Left);
 
-        
-
-        // Input Examples
         if (rightInput != null)
         {
             if (rightInput.GetButtonDown(VRButton.Back))
@@ -51,7 +50,6 @@ public class MovementTest : MonoBehaviour
                 moveRight = !moveRight;
                 moveLeft = false;
             }
-                
 
             if (rightInput.GetButtonDown(VRButton.One))
             {
@@ -59,66 +57,51 @@ public class MovementTest : MonoBehaviour
                 moveLeft = !moveLeft;
                 moveRight = false;
             }
-                
         }
-
-        if (leftInput != null)
-        {
-            if (leftInput.GetButtonDown(VRButton.Back))
-                Debug.Log("Back button pressed");
-
-            if (leftInput.GetButtonDown(VRButton.One))
-                Debug.Log("Trigger button pressed");
-        }
-
-        // Any input
-        // VRDevice.Device.GetButtonDown(VRButton.One);
         
-
+        //Any input
+        //VRDevice.Device.GetButtonDown(VRButton.One);
     }
 
     private void FixedUpdate()
     {
-        
-        
-
         if (moveRight)
         {
             paperPlane.position += Vector3.right * moveValue * Time.deltaTime;
-            paperPlane.Rotate(0, -rotationSpeed * Time.deltaTime, 0, Space.Self);
-
+            if (shouldRotate)
+                paperPlane.Rotate(0, -rotationSpeed * Time.deltaTime, 0, Space.Self);
         }
         if (moveLeft)
         {
             paperPlane.position += Vector3.left * moveValue * Time.deltaTime;
-            paperPlane.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.Self);
-            
+            if (shouldRotate)
+                paperPlane.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.Self);
         }
         if (!moveLeft && !moveRight)
         {
             //StartCoroutine(SmoothRotationReset());
-            //paperPlane.rotation.eulerAngles = Vector3.MoveTowards(currentRotation, neutralRotation, 45f);
         }
 
         paperPlane.position += Vector3.forward * speed * Time.deltaTime;
 
-        //if (paperPlane.rotation.y >= 45)
-        //{
-        //    paperPlane.rotation = new Quaternion quaternion (0, 45, 0);
-        //}
+        if (paperPlane.rotation.y >= 45 || paperPlane.rotation.y <= -45)
+            shouldRotate = false;
+        else
+            shouldRotate = true;
     }
 
-    IEnumerator SmoothRotationReset()
+    private IEnumerator SmoothRotationReset()
     {
         Vector3 currentRotation = paperPlane.localRotation.eulerAngles;
         Vector3 deltaRotation = neutralRotation - currentRotation;
+
         if (currentRotation == neutralRotation)
         {
             yield break;
         }
         
         float timeDelta = 0.0f;
-        while(timeDelta < 1.0f)
+        while(timeDelta < 2.0f)
         {
             timeDelta += Time.deltaTime;
             paperPlane.Rotate(deltaRotation * Time.deltaTime, Space.Self);
@@ -152,8 +135,4 @@ public class MovementTest : MonoBehaviour
         ScreenFader.Instance.FadeToClear(duration: 1);
     }
 
-    public void ChangeCubeSize()
-    {
-        Cube.localScale = Vector3.one * Random.Range(0.1f, 0.35F);
-    }
 }
