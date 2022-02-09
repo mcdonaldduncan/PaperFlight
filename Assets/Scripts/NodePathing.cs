0fea77f06413c222a76152d3f48eccadd351ba1d
mcdonaldduncan <380939@colum.edu>
2022-02-09 13:43:59 -0600

    add pathfinding mechanic
    absolute basics of pathfinding

---
+++ Assets/Scripts/NodePathing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodePathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> nodes = new List<Transform>();
+    [SerializeField] GameObject paperPlane;
+    [SerializeField] float speed;
+
+    int nodeIndex;
+
+    void Start()
+    {
+        nodeIndex = 0;
+    }
+
+    void Update()
+    {
+        
+
+    }
+
+    void FixedUpdate()
+    {
+        if (nodeIndex == nodes.Count)
+        {
+            return;
+        }
+
+        float step = speed * Time.deltaTime;
+        paperPlane.transform.position = Vector3.MoveTowards(paperPlane.transform.position, nodes[nodeIndex].transform.position, step);
+        paperPlane.transform.right = -nodes[nodeIndex].transform.position;
+
+        if (Vector3.Distance(paperPlane.transform.position, nodes[nodeIndex].transform.position) < .001f)
+        {
+            nodeIndex++;
+        }
+    }
+}
