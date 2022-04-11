e9e027df8645d04b0d2e8429f0a1a4013b56efcc
cindytwi <94187203+TsukinoTwilight@users.noreply.github.com>
2022-04-11 17:00:56 -0500

    waterfall attempt

---
+++ Assets/Materials/Waterfall/animatedWater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animatedWater : MonoBehaviour
+{
+    public float speedX = 0.1f;
+    public float speedY = 0.1f;
+    private float curX;
+    private float curY;
+   
+    void Start()
+    {
+        curX = GetComponent<Renderer>().material.mainTextureOffset.x;
+        curY = GetComponent<Renderer>().material.mainTextureOffset.y;
+    }
+
+   
+    void FixedUpdate()
+    {
+        curX += Time.deltaTime * speedX;
+        curY += Time.deltaTime * speedY;
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(curX, curY));
+    }
+}
