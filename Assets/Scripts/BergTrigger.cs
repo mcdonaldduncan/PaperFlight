42160c295473c7bd32effdb0c589f6ed1ec8afe3
Brian R <ryztiq@gmail.com>
2022-04-13 14:50:14 -0500

    Fixed Iceberg Animation

---
+++ Assets/Scripts/BergTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BergTrigger : MonoBehaviour
+{
+    Bounds colBounds;
+    public bool funny = false;
+    [SerializeField] Collider col;
+    [SerializeField] private Animator myAnimator;
+    [SerializeField] Transform plane;
+
+    void Start()
+    {
+        funny = false;
+        colBounds = col.bounds;
+    }
+
+    private void Update()
+    {
+        if (colBounds.Contains(plane.position))
+            myAnimator.SetBool("Berg", true);
+        funny = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            funny = true;
+            myAnimator.SetBool("Berg", true);
+        }
+    }
+}
