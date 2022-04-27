ca6bf8e560636bdf3026e19f93396d6974f2228e
mcdonaldduncan <380939@colum.edu>
2022-04-27 18:46:45 -0500

    Audio Work

---
+++ Assets/Scripts/Audio/PeterAudio.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeterAudio : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] AudioSource pianoHarmony;
+    [SerializeField] AudioSource stringsEnsemble;
+    [SerializeField] AudioSource pianoSparks;
+    [SerializeField] AudioSource fluteMelody;
+    [SerializeField] AudioSource bassoonMelody;
+    [SerializeField] AudioSource hornEnsemble;
+    [SerializeField] Collider caveCollider;
+    [SerializeField] Collider lakeCollider;
+    [SerializeField] Collider mountainCollider;
+    [SerializeField] Collider birdCollider;
+    [SerializeField] float fadeSpeed;
+
+    float fullVolume = 1;
+    float minVolume = 0;
+
+    Bounds caveBounds;
+    Bounds lakeBounds;
+    Bounds mountainBounds;
+    Bounds birdBounds;
+
+    void Start()
+    {
+        caveBounds = caveCollider.bounds;
+        mountainBounds = mountainCollider.bounds;
+        lakeBounds = lakeCollider.bounds;
+        birdBounds = birdCollider.bounds;
+
+        hornEnsemble.volume = minVolume;
+        bassoonMelody.volume = minVolume;
+        fluteMelody.volume = minVolume;
+        pianoSparks.volume = minVolume;
+        pianoHarmony.volume = fullVolume;
+        stringsEnsemble.volume = fullVolume;
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = target.position;
+        }
+
+        CheckCaveStart();
+        CheckLakeStart();
+        CheckBirdStart();
+        CheckMountainStart();
+    }
+
+    void CheckCaveStart()
+    {
+        if (pianoSparks.volume != minVolume)
+            return;
+        if (caveBounds.Contains(transform.position))
+        {
+            StartCoroutine(FadeAudio(pianoSparks, fullVolume));
+            StartCoroutine(FadeAudio(stringsEnsemble, minVolume));
+            StartCoroutine(FadeAudio(fluteMelody, fullVolume));
+        }
+    }
+
+    void CheckLakeStart()
+    {
+        if (pianoSparks.volume != fullVolume)
+            return;
+        if (lakeBounds.Contains(transform.position))
+        {
+            StartCoroutine(FadeAudio(fluteMelody, minVolume));
+            StartCoroutine(FadeAudio(stringsEnsemble, fullVolume));
+        }
+    }
+
+    void CheckBirdStart()
+    {
+        if (stringsEnsemble.volume != fullVolume)
+            return;
+        if (birdBounds.Contains(transform.position))
+        {
+            StartCoroutine(FadeAudio(stringsEnsemble, minVolume));
+            StartCoroutine(FadeAudio(bassoonMelody, fullVolume));
+        }
+    }
+
+    void CheckMountainStart()
+    {
+        if (mountainBounds.Contains(transform.position))
+        {
+            StartCoroutine(FadeAudio(hornEnsemble, fullVolume));
+            StartCoroutine(FadeAudio(stringsEnsemble, fullVolume));
+            StartCoroutine(FadeAudio(pianoSparks, minVolume));
+            StartCoroutine(FadeAudio(pianoHarmony, minVolume));
+            StartCoroutine(FadeAudio(bassoonMelody, minVolume));
+        }
+    }
+
+    IEnumerator FadeAudio(AudioSource audioToAdjust, float volume)
+    {
+        while (audioToAdjust.volume != volume)
+        {
+            float step = fadeSpeed * Time.deltaTime;
+            audioToAdjust.volume = Mathf.MoveTowards(audioToAdjust.volume, volume, step);
+            yield return null;
+        }
+
+        yield break;
+    }
+}
