54c65e4ea08105c91de0e0d44563f2ab9ff0fdda
mcdonaldduncan <380939@colum.edu>
2022-05-06 00:19:09 -0500

    move aurora at end

--- Assets/Scripts/End.cs
+++ Assets/Scripts/End.cs
@@ -8,7 +8,10 @@ public class End : MonoBehaviour
 {
     [SerializeField] private GameObject EndUI;
     [SerializeField] private GameObject Game;
+    [SerializeField] Transform aurora;
+    [SerializeField] Transform target;
     [SerializeField] private bool FadeEnd;
+    
 
     private void OnTriggerEnter(Collider other)
     {
@@ -21,6 +24,7 @@ public class End : MonoBehaviour
     {
         ScreenFader.Instance.FadeTo(Color.black, duration: 1);
         yield return ScreenFader.Instance.WaitUntilFadeComplete();
+        MoveAurora();
         Game.SetActive(false);
 
         if(!FadeEnd)
@@ -28,6 +32,11 @@ public class End : MonoBehaviour
             ScreenFader.Instance.FadeToClear(duration: 1);
             EndUI.SetActive(true);
         }
+    }
+
+    void MoveAurora()
+    {
+        aurora.position = new Vector3(aurora.position.x, aurora.position.y, target.position.y);
     }
 
     public void EndGame()
