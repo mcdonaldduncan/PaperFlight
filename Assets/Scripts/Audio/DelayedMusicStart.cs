8a1ff5b1d25a33b622f04047b38d72769d7a416e
mcdonaldduncan <380939@colum.edu>
2022-05-07 22:17:35 -0500

    small scene tweaks and lighting

---
+++ Assets/Scripts/Audio/DelayedMusicStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedMusicStart : MonoBehaviour
+{
+    [SerializeField] AudioSource music;
+    [SerializeField] float delay;
+
+    WaitForSeconds audioDelay;
+
+
+    void Start()
+    {
+        audioDelay = new WaitForSeconds(delay);
+        StartCoroutine(StartAfterDelay());
+    }
+
+    IEnumerator StartAfterDelay()
+    {
+        yield return audioDelay;
+        music.Play();
+    }
+}
