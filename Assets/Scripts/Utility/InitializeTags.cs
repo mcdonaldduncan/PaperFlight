b7cc67a540471ad1311138052d8adc04ea4760a0
David Nichol <5cents87@gmail.com>
2022-05-08 23:55:36 -0500

    Clean up files 1

---
+++ Assets/Scripts/Utility/InitializeTags.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeTags : MonoBehaviour
+{
+    [SerializeField] private GameObject cave;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject birds;
+    [SerializeField] private GameObject lake;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        birds.gameObject.tag = "Birds";
+        player.gameObject.tag = "Player";
+        lake.gameObject.tag = "Lake";
+        cave.gameObject.tag = "Cave";
+        this.gameObject.SetActive(false);
+    }
+}
